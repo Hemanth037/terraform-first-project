1be941ff0c6c46a924fad86d6d70045bb3e71c96
hemanthkatta07@gmail.com <hemanthkatta07@gmail.com>
2025-07-16 09:58:34 +0000

    spoiledtwo

--- resource.tf
+++ resource.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "storesterra" {
  
 resource "azurerm_storage_account" "saterra" {
     name                     = "storageterrahk07"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
+    resource_group_name      = azurerm_resource_group.storesterra.name
+    location                 = azurerm_resource_group.storesterra.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
